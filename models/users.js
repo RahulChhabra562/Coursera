069595309156b7674793d8f04f243a4f49a55ce7
RahulChhabra562 <chhabra.rahul562@gmail.com>
2020-06-21 03:00:28 +0530

    Passport Local authentication

--- models/users.js
+++ models/users.js
@@ -1,22 +1,19 @@
 const mongoose = require('mongoose');
 
+
+var passportLocalMongoose = require('passport-local-mongoose');
+
 const Schema = mongoose.Schema;
 
 var User = new Schema({
-    username: {
-        type: String,
-        unique: true,
-        required: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
     admin: {
         type: Boolean,
         default: false
     }
 
+
 });
+
+User.plugin(passportLocalMongoose);
 
 module.exports =  mongoose.model('User', User);
